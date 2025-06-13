ef92cf4c550d23d182ed6f93593ae824a5ebb1e6
MarekKnobloch <marek.knobloch.024@pslib.cz>
2025-06-13 13:33:54 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,26 +3,59 @@ type IRC = {
     c: DigitalPin,
     r: DigitalPin
 }
-type PinData = {
-    l: boolean,
-    c: boolean,
-    r: boolean
-}
+
 const IR: IRC = {
-    l: DigitalPin.P14,
-    c: DigitalPin.P15,
+    l: DigitalPin.P15,
+    c: DigitalPin.P8,
     r: DigitalPin.P13
 }
+
 pins.setPull(IR.l, PinPullMode.PullNone);
 pins.setPull(IR.c, PinPullMode.PullNone);
 pins.setPull(IR.r, PinPullMode.PullNone);
 
-let data: PinData = {l: false, c: false, r: false};
 basic.forever(function () {
-    data.l = pins.digitalReadPin(IR.l) === 1;
-    data.c = pins.digitalReadPin(IR.c) === 1;
-    data.r = pins.digitalReadPin(IR.r) === 1;
-    console.log(data);
+    let left = pins.digitalReadPin(IR.l)
+    let center = pins.digitalReadPin(IR.c)
+    let right = pins.digitalReadPin(IR.r)
+})
 
-    basic.pause(20)
+
+function forward() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 150)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 150)
+}
+function turnLeft() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 120)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 10)
+}
+function turnRight() {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 10)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 120)
+}
+function stop() {
+    PCAmotor.MotorStopAll()
+}
+
+basic.forever(function () {
+    let left = pins.digitalReadPin(IR.l)
+    let center = pins.digitalReadPin(IR.c)
+    let right = pins.digitalReadPin(IR.r)
+
+    // if (center == 0 && left == 1 && right == 1) {
+    //     forward()
+    // } else if (left == 0) {
+    //     turnRight()
+    // } else if (right == 0) {
+    //     turnLeft()
+    // } else {
+    //     stop()
+    // }
+    if (center == 0) {
+        forward()
+    } else {
+        stop()
+    }
+
+    basic.pause(10)
 })
